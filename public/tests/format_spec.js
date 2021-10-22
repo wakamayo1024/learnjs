3e360f3e353d8721d1e9c4f5320826df92acaf58
atmark <atmark@atde7>
2021-10-22 11:25:43 +0900

    3913

--- public/tests/format_spec.js
+++ public/tests/format_spec.js
@@ -1,8 +1,41 @@
-var formattedProblem = [];
-learnjs.problems.forEach(function(problem) {
-    formattedProblem.push({
-        code: learnjs.formatCode(problem.code),
-        name: problem.name
+learnjs.formatCode = function(obj) {
+    return obj;
+}
+
+function formatProblems() {
+    var formattedProblem = [];
+    learnjs.problems.forEach(function(problem) {
+        formattedProblem.push({
+            code: learnjs.formatCode(problem.code),
+            name: problem.name
+        });
+    });
+    return formattedProblem;
+}
+
+function betterFormatProblems() {
+    return learnjs.problems.map(learnjs.formatCode);
+}
+
+describe('betterFormatProblems', function() {
+    beforeEach(function() {
+        spyOn(learnjs, 'formatCode').and.callFake(function(problem) {
+            return {
+                code: "formatted",
+                name: problem.name
+            };
+        });
+    });
+    it('aplies a formatter to all the problems', function() {
+        expect(betterFormatProblems()[0].code).toEqual("formatted");
     });
 });
-return formattedProblem;
+
+describe('formatProblems', function() {
+    beforeEach(function() {
+        spyOn(learnjs, 'formatCode').and.returnValue("formatted");
+    });
+    it('applies a formatter to all the problems', function() {
+        expect(formatProblems()[0].code).toEqual("formatted");
+    });
+});

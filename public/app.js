da68a459bc27042320e0fa19139b171486e035e5
atmark <atmark@atde7>
2021-10-28 09:11:48 +0900

    3000-Added-problem-view-template.patch

--- public/app.js
+++ public/app.js
@@ -3,8 +3,9 @@
 var learnjs = {};
 
 learnjs.problemView = function(problemNumber) {
-  var title = 'Problem #' + problemNumber + ' Coming soon!';
-  return $('<div class="problem-view">').text(title);
+  var view = $('.templates .problem-view').clone();
+  view.find('.title').text('Problem #' + problemNumber);
+  return view;
 }
 
 // START: showView

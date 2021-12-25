aa6a93a7dc5a195e57ddc34545be5f75dc8db349
mayo <mayo@mywk.local>
2021-12-25 20:08:41 +0900

    6100

--- services/lib/index.js
+++ services/lib/index.js
@@ -1,3 +1,34 @@
+var http = require('http');
+var AWS = require('aws-sdk');
+
+AWS.config.region = 'ap-northeast-1'
+
+var config = {
+  dynamoTableName: 'learnjs',
+};
+
+exports.dynamodb = new AWS.DynamoDB.DocumentClient();
+
+function reduceItems(memo, items) {
+  items.forEach(function(item) {
+    memo[item.answer] = (memo[item.answer] || 0) + 1;
+  });
+  return memo;
+}
+
+exports.popularAnswers = function(json, context) {
+  exports.dynamodb.scan({
+    Key: {problemNumber: json.problemNumber},
+    TableName: config.dynamoTableName
+  }, function(err, data) {
+    if(err) {
+      context.fail(err);
+    } else {
+      context.succeed(reduceItems({}, data.Items));
+    }
+  });
+};
+
 exports.echo = function(json, context) {  
   context.succeed(["Hello from the cloud! You sent " + JSON.stringify(json)]);
 };
--- services/spec/index_spec.js
+++ services/spec/index_spec.js
@@ -3,7 +3,8 @@ describe('lambda function', function() {
   var context;
 
   beforeEach(function() {
-    context = jasmine.createSpyObj('context', ['succeed']);
+    context = jasmine.createSpyObj('context', ['succeed', 'fail']);
+    index.dynamodb = jasmine.createSpyObj('dynamo', ['scan']);
   });
 
   describe('echo', function() {
@@ -11,6 +12,34 @@ describe('lambda function', function() {
       index.echo({}, context);
       expected = ["Hello from the cloud! You sent {}"];
       expect(context.succeed).toHaveBeenCalledWith(expected);
+    });
+  });
+
+  // 6100
+  describe('popularAnswers', function() {
+    it('requests problems with the given problem number', function() {
+      index.popularAnswers({problemNumber: 42}, context);
+      expect(index.dynamodb.scan).toHaveBeenCalledWith({
+        Key: {problemNumber: 42},
+        TableName: 'learnjs'
+      }, jasmine.any(Function));
+    });
+    it('group answers by minified code', function() {
+      index.popularAnswers({problemNumber: 1}, context);
+      index.dynamodb.scan.calls.first().args[1](undefined, {Items: [
+        {answer: "true"},
+        {answer: "true"},
+        {answer: "true"},
+        {answer: "!false"},
+        {answer: "!false"},
+      ]});
+      expect(context.succeed).toHaveBeenCalledWith({"true": 3, "!false": 2});
+    });
+
+    it('fails the request if dynamo return an error', function() {
+      index.popularAnswers({problemNumber: 1}, context);
+      index.dynamodb.scan.calls.first().args[1]('error');
+      expect(context.fail).toHaveBeenCalledWith('error');
     });
   });
 });
